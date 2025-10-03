bd5807bd5cafaada9161ad089dd5b6b9154412a7
G4M37Z <linyuluderrick@gmail.com>
2025-10-03 20:14:38 +0300

    chore: align Vercel build settings and Node version

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Do NOT set experimental appDir if you’re using Pages Router
+  // experimental: { appDir: false },
+}
+module.exports = nextConfig
